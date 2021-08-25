c431120de171be23c9e8cf9bafda65ff7d84835b
Miguel <miguel.perezf@upb.edu.co>
2021-08-24 21:45:41 -0500

    Feature 1 terminado

--- main.c
+++ main.c
@@ -4,5 +4,27 @@
 
 int main(int argc, char *argv[]){
 
+    if(argc != 3){
+        printf("Se deben pasar obligatoriamente 3 argumentos");
+        return(EXIT_FAILURE);
+    }
+
+    FILE *fin = fopen(argv[1],"r");
+    if (fin == NULL){
+        perror("Falla en la apertura del archivo de entrada: ");
+        return(EXIT_FAILURE);
+    }
+
+    FILE *fout = fopen(argv[2],"a+");
+    if (fout == NULL){
+        perror("Falla en la apertura del archivo de salida: ");
+        return(EXIT_FAILURE);
+    }
+
+    feature1(fin, fout);
+    
+
+
+
     return EXIT_SUCCESS;
 }
--- student.c
+++ student.c
@@ -1,3 +1,38 @@
 #include <stdlib.h>
 #include "student.h"
+#include <string.h>
+
+void feature1(FILE *fin, FILE *fout){
+
+    char buffer[64];
+    char *status =  NULL;
+    int cont = 0; //Para controlar el ciclo que se haga solo para leer la primera linea del archivo
+    do{
+        status = fgets(buffer, sizeof(buffer),fin);
+        cont++;
+        if(status != NULL){
+            //printf("%s",buffer);
+            fputs(buffer, fout);
+        }
+    }while (cont <= 0);
+    printf("\n");
+
+}
+
+void feature2(FILE *fin, FILE *fout){ }
+
+void feature3(FILE *fin, FILE *fout){ }
+
+void feature4(FILE *fin, int **parr, int *length, char **op){ }
+
+void feature5(FILE *fout, int *parr, int length, char *op){ }
+
+void feature6(FILE *fin, struct Obj_t *pobj){ }
+
+void feature7(FILE *fout, struct Obj_t *pobj){ }
+
+void feature8(FILE *fin, struct Obj_t **pobj,int *length){ }
+
+void feature9(FILE *fout, struct Obj_t *pobj,int length){ }
+
 
