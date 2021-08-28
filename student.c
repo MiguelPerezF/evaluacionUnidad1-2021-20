b8946ffcc6f6253aa54320f244aa9c2da39c6366
Miguel <miguel.perezf@upb.edu.co>
2021-08-27 23:46:17 -0500

    Feature 3 terminado
    Se realizaron ajustes en feature 2 para tener en cuenta el salto de linea y se rediseño feature 3 para funcionar con variaciones en la cadena de entrada

--- student.c
+++ student.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "student.h"
 #include <string.h>
+#include <ctype.h>
+
 
 void feature1(FILE *fin, FILE *fout){
 
@@ -23,15 +25,24 @@ void feature2(FILE *fin, FILE *fout){
     char buffer2[256];
     char buffinv[256];
     char *status2 =  NULL;
+    char salto = 10; //10 es una nueva linea en ASCII
     int cont2 = 0; //Para controlar el ciclo que se haga solo para leer la primera linea del archivo
     do{
         status2 = fgets(buffer2, sizeof(buffer2),fin);
         cont2++;
         if(status2 != NULL){
             for(int i = strlen(buffer2); i >= 0; i--){
+                if (buffer2[i] == 10)
+                {
+                    salto = buffer2[i]; //guardo el '10' del salto en ASCII en la variable salto
+                }
                 if(buffer2[i] != 0 || buffer2[i] != 10) //evitar el 0 y el salto de linea: 00 y 10 en ASCII
-                buffinv[strlen(buffer2)-i-2] = buffer2[i];
+                buffinv[strlen(buffer2)-i-2] = buffer2[i]; //-2 para evitar el 0 del fin de la cadena y el salto de linea
                 //printf("Array inverso en posicion %ld: %d\n", (strlen(buffer2) - i), buffer2[i]);
+                if (i == strlen(buffer2)-1)
+                {
+                    buffinv[i] = buffer2[i]; //agrego el salto de lìnea en el arreglo para que feature3 escriba en la siguiente linea
+                }
             }
             fputs(buffinv, fout);
         }
@@ -40,79 +51,35 @@ void feature2(FILE *fin, FILE *fout){
 }
 
 void feature3(FILE *fin, FILE *fout){ 
-    char buffer3[256];
-    char aux1[256];
-    int arrterm[256];
-    int valact = 0;
-    int valsig = 0;
-    int val2sig = 0;
-    int suma = 0;
+    char buffer3[128];
+    char *token;
     char *status3 =  NULL;
+    int suma = 0;
     int cont3 = 0; //Para controlar el ciclo que se haga solo para leer la primera linea del archivo
-    int posaux = 0;
     do{
         status3 = fgets(buffer3, sizeof(buffer3),fin);
         cont3++;
         if(status3 != NULL){
-            for (int i = 0; i < strlen(buffer3); i++)
+            token = strtok(buffer3, " "); //strtok es para partir una cadena de caracteres en subcadenas indicando el separador
+            if (token == NULL)
             {
-                valact = buffer3[i] - 48;
-                valsig = buffer3[i+1] - 48;
-                val2sig = buffer3[i+2] - 48;
-                printf("Valor actual: %d\n", valact);
-                if (valact > 0)
-                {
-                    if (valsig >= 0)
-                    {
-                        valact = (((valact)*10)+valsig);
-                        aux1[posaux] = valact;
-                        posaux++;
-                        i++;
-                    }
-                    else{
-                        aux1[posaux] = valact;
-                        posaux++;
-                    }
-                }
-                else{
-                    if (valact == -3)
-                    {
-                        if (val2sig >= 0)
-                        {
-                            valact = (((valsig)*10)+val2sig)*(-1);
-                            aux1[posaux] = valact;
-                            posaux++;
-                            i+=2;
-                        }
-                        else{ 
-                            valact = (valsig)*(-1);
-                            aux1[posaux] = valact;
-                            posaux++;
-                            i++;
-                        }   
-                    }
-                    else{
-                        valact = -100; //Distintivo para cuando es un espacio
-                        aux1[posaux] = valact;
-                        posaux++;
-                    }
-                }
+                printf("No hay elementos en el arreglo");
+                return (EXIT_FAILURE);
             }
-        }              
-    
+            suma+=atoi(token); //atoi es para convertir a entero el char que tiene token
+            while (token != NULL)
+            {
+                token = strtok(NULL, " ");
+                if (token != NULL)
+                {
+                    suma+=atoi(token);
+                }    
+            }
+            //printf("La suma total es: %d\n", suma);
+            fprintf(fout, "%d", suma);
+        }                  
     }while (cont3 <= 0);
     printf("\n");
-    for (int i = 0; i < strlen(buffer3); i++)
-    {
-        if (aux1[i] != -100) //Sumo las casillas que no sean -100 (porque -100 significa espacio)
-        {
-            suma+=aux1[i];
-        }
-        
-        printf("Arr(%d): %d\n", i, aux1[i]);
-        printf("La suma es: %d\n", suma);
-
-    }
 }
 
 void feature4(FILE *fin, int **parr, int *length, char **op){ }
